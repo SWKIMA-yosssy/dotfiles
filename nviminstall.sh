c8d477f9346969d765694f9f60a7f37d258127a2
SWKIMA <greyonth@gmail.com>
2024-01-05 12:58:23 +0000

    make two shellscript

---
+++ nviminstall.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#because of the necessity to install neovim 0.8+ for deno and ddc.vim's require do not use apt in installing
+cd ${HOME}
+#this is version 0.9.5
+wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
+if [ ! -d ${HOME}/.local/bin]
+then
+	mkdir -p /.local/bin
+fi
+mv nvim-linux64.tar.gz ${HOME}/.local/bin
+cd .local/bin
+tar xzvf nvim-linux64.tar.gz
+rm -fr nvim-linux64.tar.gz
+ln -s ./nvim-linux64/bin/nvim ./nvim
+cd ${HOME}
+#End of Install and come back to home dir
+
+#install vim-plug
+yes|apt install curl
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+#install deno to use ddc.vim
+yes|apt install unzip #this need to do following line
+curl -fsSL https://deno.land/install.sh | sh
+
+#put symbolic link
+if [ ! -d ${HOME}/.config/nvim]
+then
+	mkdir -p ${HOME}/.config/nvim
+fi
+ln -s ${HOME}/dotfiles/.config/nvim/init.vim ${HOME}/.config/nvim/init.vim
+nvim -c PlugInstall -c q -c q
+
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update and upgrade of apt setting all "yes"
+yes|apt update
+yes|apt -y upgrade
+if [ ! -d ${HOME}/dotfiles]
+then #if dotfiles does not exist in homedir.
+	echo "error:dotfiles does not exists proper position, must be home/USER/dotfiles"
+else
+	cd ${HOME}/dotfiles
+	./nviminstall.sh
+fi
+
