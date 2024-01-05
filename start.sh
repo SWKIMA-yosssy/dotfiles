e3ac0825497b7bdbc4e7af7d7ba6a62b67616180
SWKIMA <146754361+SWKIMA-yosssy@users.noreply.github.com>
2024-01-06 00:20:34 +0900

    Update start.sh
    insert code for deno

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ then #if dotfiles does not exist in homedir.
 	echo "error:dotfiles does not exists proper position, must be home/USER/dotfiles"
 else
 	cd ${HOME}/dotfiles
+ 	#here is necessary to install deno properly
+ 	echo "export DENO_INSTALL=${HOME}/.deno" >> ${HOME}/.bashrc
+  	echo 'export PATH="$DENO_INSTALL/bin:$PATH"' >> ${HOME}/.bashrc
+	source ~/.bashrc
+ 	#Code for deno's install end
 	./nviminstall.sh
 fi
 
