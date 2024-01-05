63812c02086b904f0a2ca424ff1ccf53ee50c9c6
SWKIMA <146754361+SWKIMA-yosssy@users.noreply.github.com>
2024-01-05 23:50:00 +0900

    Update start.sh
    insert space befoer ]

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # update and upgrade of apt setting all "yes"
 yes|apt update
 yes|apt -y upgrade
-if [ ! -d ${HOME}/dotfiles]
+if [ ! -d ${HOME}/dotfiles ]
 then #if dotfiles does not exist in homedir.
 	echo "error:dotfiles does not exists proper position, must be home/USER/dotfiles"
 else
